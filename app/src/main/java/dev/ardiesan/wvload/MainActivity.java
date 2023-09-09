c2e93ffa9063d8e31de45f4bf1b35ffcac80e5c2
ardiesan <113565882+ardiesan@users.noreply.github.com>
2023-09-09 10:16:27 +0800

    Initial commit, working Local Content WebView Loader

---
+++ app/src/main/java/dev/ardiesan/wvload/MainActivity.java
@@ -0,0 +1,37 @@
+package dev.ardiesan.wvload;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.webkit.WebViewAssetLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView = null;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+
+        WebSettings webViewSettings = webView.getSettings();
+        webViewSettings.setJavaScriptEnabled(true);
+        webViewSettings.setAllowFileAccess(true);
+        webViewSettings.setAllowContentAccess(true);
+        webViewSettings.setAllowFileAccessFromFileURLs(true);
+
+        final WebViewAssetLoader assetLoader = new WebViewAssetLoader.Builder()
+                .addPathHandler("/assets/", new WebViewAssetLoader.AssetsPathHandler(this))
+                .build();
+        LocalContentWebViewClient webViewClient = new LocalContentWebViewClient(assetLoader);
+        webView.setWebViewClient(webViewClient);
+
+        webView.loadUrl("https://ardiesan.github.io");
+    }
+}
